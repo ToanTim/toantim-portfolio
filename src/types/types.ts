499b617474383803f86013fb5b4446ebc1e7c713
Toan Tran <toantim.developer@gmail.com>
2026-01-10 12:17:54 +0200

    New feature: project overview at homepage and project detail by id page. Create types ProjectOverview, ProjectData, Step

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,35 @@
-123;
+/* ./projects/[id] */
+interface Step {
+  id: string;
+  title: string;
+  icon: string;
+  content: {
+    engineering: string;
+  };
+}
+
+interface ProjectData {
+  id: string;
+  title: string;
+  description: string;
+  image?: string;
+  gradient: string;
+  tech: string[];
+  active: boolean;
+  steps: Step[];
+  categories?: string;
+  tags?: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ProjectOverview {
+  id: string;
+  title: string;
+  description: string;
+  tech: string[];
+  gradient?: string;
+  image?: string;
+};
+
+export type { ProjectOverview,ProjectData, Step };
