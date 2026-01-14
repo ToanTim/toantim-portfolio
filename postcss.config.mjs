c2983a79436f011edc7ce27791e49a2050c06e58
Toan Tran <toantim.developer@gmail.com>
2026-01-14 09:26:49 +0200

    FIX: test production level if the style stable after update files

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
+//File that might affect styling in production level if change 
+//✅ Production-safe
+//✅ Works with Next.js App Router
+//✅ Compatible with Vercel
+//✅ Fixes missing styles
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
