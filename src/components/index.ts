f8bf143de2754963c1e7f9950b51b2be9edb7e48
Toan Tran <toantim.developer@gmail.com>
2026-01-14 12:08:42 +0200

    FEATURE: loading screen for initial access

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Navbar } from "./Navbar";
 export { default as ResearchCard } from "./ResearchCard";
 export { default as ImageSwiper } from "./ImageSwiper";
+export { default as PageLoader } from "./PageLoader";
 
 // Skeleton loaders
 export * from "./skeletons";
